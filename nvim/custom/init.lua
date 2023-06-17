192ca610ad8de5ad154de1d8fe2343c8229d4c26
Miguel <decode380@gmail.com>
2023-06-16 21:03:07 -0500

    nvimchad and tmux

---
+++ nvim/custom/init.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_create_user_command('CtmInit', ':e ~/.config/nvim/lua/custom/init.lua', {})
+vim.api.nvim_create_user_command('CtmPlug', ':e ~/.config/nvim/lua/custom/plugins.lua', {})
+vim.api.nvim_create_user_command('CtmLsp', ':e ~/.config/nvim/lua/custom/configs/lspconfig.lua', {})
+
---
+++ nvim/custom/mappings.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.general_custom = {
+  i = {
+    ["ii"] = {"<Esc>", "Escape insert mode"},
+  },
+  n = {
+    ["o"] = {"o<Esc>", "Escape insert new line down"},
+    ["O"] = {"O<Esc>", "Escape insert new line up"},
+  },
+}
+
+return M
